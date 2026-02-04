cd44e25889406b90eb0c007191bdaa1001fd7798
YoLaub <johngreta904@gmail.com>
2026-02-05 00:16:36 +0100

    feat: header

--- src/app/(front)/_components/Header.tsx
+++ src/app/(front)/_components/Header.tsx
@@ -3,25 +3,38 @@ import { Navbar } from "./Navbar";
 
 export function Header() {
     return (
-        <header className="sticky top-0 z-50 w-full border-b border-zinc-200 bg-white/80 backdrop-blur-md dark:border-zinc-800 dark:bg-black/80">
-            <div className="container mx-auto flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
+        <header className="relative h-100 border-b border-zinc-200 dark:border-zinc-800 overflow-hidden">
+            <div className="absolute inset-0 -z-10 ">
+                <img
+                    src="/Header.webp"
+                    alt="Background Header"
+                    className=" w-full object-cover object-center"
+                />
+            </div>
+
+            <div className=" relative sticky top-0 z-50 container mx-auto mt-5 flex h-16 items-center justify-end gap-90 px-4 sm:px-6 lg:px-8 ">
                 <div className="flex items-center gap-8">
-                    <Link href="/" className="text-xl font-bold tracking-tighter">
-                        LES GANTS MÉLECIENS
-                    </Link>
                     <Navbar />
+
                 </div>
 
                 <div className="flex items-center gap-4">
-                    {/* Ici on pourra ajouter un bouton d'appel à l'action ou un login */}
                     <Link
                         href="/adhesion"
-                        className="rounded-full bg-black px-4 py-2 text-sm font-medium text-white transition-transform hover:scale-105 dark:bg-white dark:text-black"
+                        className="rounded-sm border-2 border-white px-4 py-2 text-lg font-medium text-white transition-transform hover:scale-105 dark:bg-white dark:text-black"
                     >
-                        Rejoindre le club
+                        Contact
                     </Link>
                 </div>
             </div>
+            <Link href="/" className="tracking-tighter">
+                <img
+                    src="/logoBlanc.webp"
+                    alt="Logo association"
+                    className="absolute top-0 h-90 w-90 object-cover object-center ms-30 mt-3"
+                />
+            </Link>
+
         </header>
     );
 }
