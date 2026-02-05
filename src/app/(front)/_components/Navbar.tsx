13e6d60683831f712b66db7fd99ab560d289a277
YoLaub <johngreta904@gmail.com>
2026-02-06 00:43:23 +0100

    fix; navBar

--- src/app/(front)/_components/Navbar.tsx
+++ src/app/(front)/_components/Navbar.tsx
@@ -10,12 +10,12 @@ const NAV_LINKS = [
 export function Navbar() {
     return (
         <nav className="hidden md:block">
-            <ul className="flex gap-8">
+            <ul className="flex gap-12">
                 {NAV_LINKS.map((link) => (
                     <li key={link.href}>
                         <Link
                             href={link.href}
-                            className="text-lg font-bold transition-colors text-white hover:text-zinc-600 dark:hover:text-zinc-300"
+                            className="text-xl font-bold transition-colors text-white hover:text-zinc-600 dark:hover:text-zinc-300"
                         >
                             {link.label}
                         </Link>
