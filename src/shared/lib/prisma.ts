c3e19d53955b4948f874cbfc29d3aee8db53520e
YoLaub <johngreta904@gmail.com>
2026-02-25 19:36:49 +0100

    chore: add `@prisma/adapter-ppg` and integrate with Prisma client
    - Replaced `@prisma/adapter-pg` with `@prisma/adapter-ppg` for database configuration.
    - Updated `package.json` and `package-lock.json` to include `@prisma/adapter-ppg`.
    - Adjusted `prisma.ts` to use the new `PrismaPostgresAdapter`.

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -1,17 +1,18 @@
 import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from '@prisma/adapter-pg';
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaPostgresAdapter } from "@prisma/adapter-ppg";
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
+// Configuration de l'adaptateur Serverless spécifique à Prisma Postgres
+const adapter = new PrismaPostgresAdapter({
+    connectionString: process.env.DATABASE_URL || '', // Ou PRISMA_DIRECT_TCP_URL selon comment tu l'as nommée
 });
 
+// Instanciation avec le bon adaptateur
 export const prisma = globalForPrisma.prisma || new PrismaClient({
     adapter,
-}).$extends(withAccelerate());
+});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
